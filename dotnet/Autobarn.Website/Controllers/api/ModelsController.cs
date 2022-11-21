53a4cf8b10e662a842a44e49dcba7182d513f5a7
Dylan Beattie <dylan@dylanbeattie.net>
2022-11-21 13:47:27 +0000

    Integrated Published component into Autobarn.Website

--- dotnet/Autobarn.Website/Controllers/api/ModelsController.cs
+++ dotnet/Autobarn.Website/Controllers/api/ModelsController.cs
@@ -1,18 +1,23 @@
-﻿using Autobarn.Data;
+using System;
+using Autobarn.Data;
 using Autobarn.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Autobarn.Messages;
 using Autobarn.Website.Models;
+using EasyNetQ;
 
 namespace Autobarn.Website.Controllers.api {
 	[Route("api/[controller]")]
 	[ApiController]
 	public class ModelsController : ControllerBase {
 		private readonly IAutobarnDatabase db;
+        private readonly IBus bus;
 
-		public ModelsController(IAutobarnDatabase db) {
-			this.db = db;
-		}
+        public ModelsController(IAutobarnDatabase db, IBus bus) {
+            this.db = db;
+            this.bus = bus;
+        }
 
 		[HttpGet]
 		public IEnumerable<Model> Get() {
@@ -43,8 +48,23 @@ namespace Autobarn.Website.Controllers.api {
                 Year = dto.Year,
                 VehicleModel = vehicleModel
             };
+
+            PublishNewVehicleMessage(vehicle);
             db.CreateVehicle(vehicle);
+
             return Created($"/api/vehicles/{vehicle.Registration}", vehicle.ToResource());
+        }
+
+        private void PublishNewVehicleMessage(Vehicle vehicle) {
+            var m = new NewVehicleMessage {
+                Registration = vehicle.Registration,
+                ManufacturerName = vehicle.VehicleModel.Manufacturer.Name,
+                ModelName = vehicle.VehicleModel.Name,
+                Color = vehicle.Color,
+                Year = vehicle.Year,
+                ListedAt = DateTimeOffset.UtcNow
+            };
+            bus.PubSub.Publish(m);
         }
 	}
 }
