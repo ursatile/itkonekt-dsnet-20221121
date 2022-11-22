88ee80ef7b2cb0242ef26dc59ae3509472c1c563
Dylan Beattie <dylan@dylanbeattie.net>
2022-11-22 11:05:38 +0000

    Adding field (LanguageCode) to protocol on client first, THEN server

--- grpc/GrpcGreeterClient/Program.cs
+++ grpc/GrpcGreeterClient/Program.cs
@@ -7,9 +7,19 @@ Console.WriteLine("Hello, World!");
 using var channel = GrpcChannel.ForAddress("https://localhost:7275");
 var client = new Greeter.GreeterClient(channel);
 Console.WriteLine("Press any key to send a gRPC request!");
-while(true) {
-    Console.ReadKey();
+Console.WriteLine("1: English GB");
+Console.WriteLine("2: English US");
+Console.WriteLine("3: English AU");
+
+while(true) {    
+    var key = Console.ReadKey(true);
+    var languageCode = key.KeyChar switch {
+        '2' => "en-US",
+        '3' => "en-AU",
+        _ => "en-GB"
+    };
     var request = new HelloRequest {
+        LanguageCode = languageCode,
         Name = "ITKonekt!"
     };
     var reply = await client.SayHelloAsync(request);
