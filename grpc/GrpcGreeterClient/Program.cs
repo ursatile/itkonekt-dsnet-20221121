389454ceb97ecc6ea51b595101249738108c73dd
Dylan Beattie <dylan@dylanbeattie.net>
2022-11-22 10:53:51 +0000

    Hello World with gRPC

---
+++ grpc/GrpcGreeterClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeter;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using var channel = GrpcChannel.ForAddress("https://localhost:7275");
+var client = new Greeter.GreeterClient(channel);
+Console.WriteLine("Press any key to send a gRPC request!");
+while(true) {
+    Console.ReadKey();
+    var request = new HelloRequest {
+        Name = "ITKonekt!"
+    };
+    var reply = await client.SayHelloAsync(request);
+    Console.WriteLine(reply);
+}
